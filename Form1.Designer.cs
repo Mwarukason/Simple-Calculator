7894d38d21a0bb2a91c8d22f07d4b7b235fb01d1
Mwaruka's Son <Mwarukason@users.noreply.github.com>
2016-11-29 05:14:50 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Simple_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.AddButton = new System.Windows.Forms.Button();
+            this.MultButton = new System.Windows.Forms.Button();
+            this.DivButton = new System.Windows.Forms.Button();
+            this.SubButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RemButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AddButton
+            // 
+            this.AddButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.AddButton.Location = new System.Drawing.Point(47, 166);
+            this.AddButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(185, 24);
+            this.AddButton.TabIndex = 0;
+            this.AddButton.Text = "ADD";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MultButton
+            // 
+            this.MultButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.MultButton.Location = new System.Drawing.Point(47, 271);
+            this.MultButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MultButton.Name = "MultButton";
+            this.MultButton.Size = new System.Drawing.Size(185, 24);
+            this.MultButton.TabIndex = 1;
+            this.MultButton.Text = "MULTPLY";
+            this.MultButton.UseVisualStyleBackColor = true;
+            this.MultButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // DivButton
+            // 
+            this.DivButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.DivButton.Location = new System.Drawing.Point(307, 271);
+            this.DivButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.DivButton.Name = "DivButton";
+            this.DivButton.Size = new System.Drawing.Size(178, 24);
+            this.DivButton.TabIndex = 2;
+            this.DivButton.Text = "DIVIDE";
+            this.DivButton.UseVisualStyleBackColor = true;
+            this.DivButton.Click += new System.EventHandler(this.DivButton_Click);
+            // 
+            // SubButton
+            // 
+            this.SubButton.BackColor = System.Drawing.SystemColors.Control;
+            this.SubButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.SubButton.Location = new System.Drawing.Point(307, 166);
+            this.SubButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.SubButton.Name = "SubButton";
+            this.SubButton.Size = new System.Drawing.Size(178, 24);
+            this.SubButton.TabIndex = 3;
+            this.SubButton.Text = "SUBTRACT";
+            this.SubButton.UseVisualStyleBackColor = false;
+            this.SubButton.Click += new System.EventHandler(this.SubButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 44);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 24);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(307, 44);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(177, 24);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(238, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 18);
+            this.label1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(188, 322);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 24);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "EXIT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(88, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 18);
+            this.label2.TabIndex = 8;
+            // 
+            // RemButton
+            // 
+            this.RemButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.RemButton.Location = new System.Drawing.Point(188, 216);
+            this.RemButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.RemButton.Name = "RemButton";
+            this.RemButton.Size = new System.Drawing.Size(175, 24);
+            this.RemButton.TabIndex = 9;
+            this.RemButton.Text = "REMINDER";
+            this.RemButton.UseVisualStyleBackColor = true;
+            this.RemButton.Click += new System.EventHandler(this.RemButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(128, 385);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(387, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Simple Calculator Designed By Mwaruka\'Son";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(527, 412);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.RemButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SubButton);
+            this.Controls.Add(this.DivButton);
+            this.Controls.Add(this.MultButton);
+            this.Controls.Add(this.AddButton);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Font = new System.Drawing.Font("Showcard Gothic", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.TransparencyKey = System.Drawing.Color.Blue;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button MultButton;
+        private System.Windows.Forms.Button DivButton;
+        private System.Windows.Forms.Button SubButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button RemButton;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
